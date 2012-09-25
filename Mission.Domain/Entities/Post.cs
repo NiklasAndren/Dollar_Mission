aca7cfd2b22eece1fbfb450c39edd0f61212274b
Niklas Andren <niklas.andren@hotmail.com>
2012-09-25 16:32:22 +0200

    Struktur v0.1

---
+++ Mission.Domain/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mission.Domain.Entities.News
+{
+    class Post
+    {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Body { get; set; }
+        public int Type { get; set; }
+    }
+}
---
+++ Mission.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mission.Domain.Entities
+{
+    public enum Role { Admin, User }
+
+    class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
