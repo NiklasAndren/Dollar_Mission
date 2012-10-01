2776c38028db446f7cd1df9d8c11c293616ef842
kalle <kalle.r.persson@gmail.com>
2012-10-01 16:42:13 +0200

    Fixat ninject och Dependency Incection v.0.5

--- Mission.WebUI/Controllers/NewsController.cs
+++ Mission.WebUI/Controllers/NewsController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mission.Domain.Repositories.Abstract;
+using Mission.Domain.Entities;
+using Mission.Domain.Repositories;
+using Mission.Domain.Entities.FakeData;
 
 namespace Mission.WebUI.Controllers
 {
     public class NewsController : Controller
     {
+        private IRepository<Post> _postRepo;
+        public NewsController(IRepository<Post> postRepo)
+        {
+            _postRepo = postRepo;
+        }
+
         //
         // GET: /News/
+
 
         public ActionResult Index()
         {
